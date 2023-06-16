fc95c680b6efe4768520dddc89c9b903a808d5f7
Alexandre Teixeira <alextp@gmail.com>
2023-06-16 01:09:56 -0300

    Adicionando cinema

---
+++ Data/dtos/ReadCinemaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmesAPI.Data.dtos
+{
+    public class ReadCinemaDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Profiles/CinemaProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Profiles
+{
+
+    public class CinemaProfile : Profile
+    {
+        public CinemaProfile()
+        {
+            CreateMap<Cinema, ReadCinemaDto>();
+            CreateMap<UpdateCinemaDto, Cinema>();
+        }
+    }
+}
