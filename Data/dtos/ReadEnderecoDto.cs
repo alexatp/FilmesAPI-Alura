735c14a88de03656d4c2680fdb242385e742fa7c
Alexandre Teixeira <alextp@gmail.com>
2023-06-17 00:10:24 -0300

    Adicionando enderecos

---
+++ Data/dtos/ReadEnderecoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesAPI.Data.dtos
+{
+    public class ReadEnderecoDto
+    {
+        public int Id { get; set; }
+        public String Logradouro { get; set; }
+        public int Numero { get; set; }
+    }
+}
---
+++ Data/dtos/UpdateEnderecoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmesAPI.Data.dtos
+{
+    public class UpdateEnderecoDto
+    {
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+    }
+}
